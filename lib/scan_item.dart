5458d9931bf799e72e089afe1c2c993a485bb389
hiroaki.inoue <hiroaki.inoue@ZTA18013.local>
2019-09-03 11:30:14 +0900

    add scan_item.dart

---
+++ lib/scan_item.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_mobile_vision/flutter_mobile_vision.dart';
+
+class ScanItem {
+  int id;
+  String text;
+
+  static const String columnId = "id";
+  static const String columnText = "text";
+
+  ScanItem.fromMap(Map map) {
+    id = map[columnId];
+    text = map[columnText];
+  }
+
+  ScanItem.fromBarcode(Barcode barcode) {
+    text = barcode.displayValue;
+  }
+
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = {columnText: text};
+    if (id != null) {
+      map[columnId] = id;
+    }
+    return map;
+  }
+}
+© 2019 GitHub, Inc.
